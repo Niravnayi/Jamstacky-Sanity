82e3d2023614b1138c2fb5f6de0ee500819c09c2
Nirav <niravlimbachia031@gmail.com>
2024-12-30 12:22:04 +0530

    schema done for home page and header

--- backend/schemaTypes/index.ts
+++ backend/schemaTypes/index.ts
@@ -1,1 +1,7 @@
-export const schemaTypes = []
+import blockContent from './blockContent'
+import header from './header'
+import page from './page'
+import section from './section'
+import heroSection from './sections/heroSection'
+
+export const schemaTypes = [header, blockContent, page, section, heroSection]
---
+++ backend/schemaTypes/sections/heroSection.ts
@@ -0,0 +1,44 @@
+import {defineType, defineField} from 'sanity'
+
+export default defineType({
+  name: 'heroSection',
+  title: 'Hero Section',
+  type: 'object',
+  fields: [
+    defineField({
+      name: 'heading',
+      title: 'Heading',
+      type: 'string',
+    }),
+    defineField({
+      name: 'subheading',
+      title: 'Subheading',
+      type: 'string',
+    }),
+    defineField({
+      name: 'buttonText',
+      title: 'Button Text',
+      type: 'string',
+    }),
+
+    defineField({
+      name: 'link',
+      title: 'Link',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'backgroundImage',
+      title: 'Background Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'text',
+      title: 'text',
+      type: 'string',
+    }),
+  ],
+})
