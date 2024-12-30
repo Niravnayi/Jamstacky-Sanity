95f0be3e826c15134f3f89c4bdea42e4a863f5bc
Nirav <niravlimbachia031@gmail.com>
2024-12-30 18:43:04 +0530

    commit 2

--- frontend/src/components/CaseStudy.tsx
+++ frontend/src/components/CaseStudy.tsx
@@ -1,9 +1,30 @@
+import { urlFor } from "@/sanity/lib/imageUrlBuilder";
+import Image from "next/image";
+import { customComponents } from "./SerializerComponent";
+import { PortableText } from "next-sanity";
+
 export default async function CaseStudy(data: any) {
   console.log(data.data);
 
   return (
     <div>
+        <h2>{data.data.title}</h2>
       <p>{data.data.description}</p>
+      <Image
+                src={
+                  urlFor(
+                    data.data.image?.asset
+                  ).url() || ""
+                }
+                alt={data.data.title}
+                width={400}
+                height={400}
+              />
+
+<PortableText
+  value={data.data.body}
+  components={customComponents}
+/>
     </div>
   );
 }
--- frontend/src/components/HeroSection.tsx
+++ frontend/src/components/HeroSection.tsx
@@ -1,3 +1,5 @@
+import { urlFor } from "@/sanity/lib/imageUrlBuilder";
+import Image from "next/image";
 
 
 export default async function HeroSection(data: any) {
@@ -6,7 +8,22 @@ export default async function HeroSection(data: any) {
   return (
     <div className="h-fit bg-[#f4f3ec] w-full p-5">
      
-     <h1>{data?.data?.subheading}</h1>
+     <h1>{data?.data?.heading}</h1>
+     <p>{data?.data?.subheading}</p>
+     <button>{data.data.buttonText1}</button>
+     <button>{data.data.buttonText2}</button>
+     <Image
+                src={
+                  urlFor(
+                   data.data.backgroundImage?.asset
+                  ).url() || ""
+                }
+                alt={data.data.heading}
+                width={400}
+                height={400}
+              />
+<p>{data.data.text}</p>
+
     </div>
   );
 }
